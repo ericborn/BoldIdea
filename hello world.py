1f5d5ac742824a11ee8e26a4e1bbec3bda99a7ae
Eric Born <eric.born85@gmail.com>
2020-05-28 10:25:34 -0700

    Update hello world.py

--- hello world.py
+++ hello world.py
@@ -1,2 +1,16 @@
 print('hello world')
+
+# error because of misspelling
 #prnt('hello world')
+
+print((3 + 4) * (15 - 9))
+
+print('my name is ' + 'eric')
+
+print('meow ' * 4)
+
+print(f'Three apples cost ${3 * 1.25}')
+
+name = 'Eric'
+
+print(f'Hello, {name}')
