66e1dda652d6fe7877dccbe4fd610315e6d17614
Eric Born <eric.born85@gmail.com>
2020-05-29 12:50:05 -0700

    Update game_project.py

--- game_project.py
+++ game_project.py
@@ -3,18 +3,22 @@ import pygame
 
 pygame.init()
 
+# constants
 FPS = 60
 BLACK = (0,0,0)
 RED = (255, 0, 0)
+BLUE = (0, 0, 255)
 
+# screen and clock setup
 screen = pygame.display.set_mode((600, 800))
 screen_rect = screen.get_rect()
 clock = pygame.time.Clock()
 
+# ball class
 class Ball(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
-        self.image = pygame.Surface((50,50))
+        self.image = pygame.Surface((20,20))
         self.image.fill(RED)
         self.rect = self.image.get_rect()
         self.rect.center = screen_rect.center
@@ -34,9 +38,23 @@ class Ball(pygame.sprite.Sprite):
         self.rect.x += self.x_speed
         self.rect.y += self.y_speed
 
+# paddle class
+class Paddle(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.Surface((100,15))
+        self.image.fill(BLUE)
+        self.rect = self.image.get_rect()
+        self.rect.center = screen_rect.center
+        self.rect.bottom = screen_rect.bottom -10
+
 all_sprites = pygame.sprite.Group()
+
 ball = Ball()
 all_sprites.add(ball)
+
+paddle = Paddle()
+all_sprites.add(paddle)
 
 running = True
 while running:
